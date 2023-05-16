2699dfbfef7473a61271c03dcb334818a4b2f558
Pierre Le Barbenchon <pierre.le-barbenchon@ens-rennes.fr>
2023-05-16 10:16:32 +0200

    modif

--- examples/draw_symbol.py
+++ examples/draw_symbol.py
@@ -9,7 +9,7 @@ path_here = os. getcwd()
 import sys
 sys.path.insert(0,path_here+"/boundaryscheme")
 
-from schemes import *
+from schemes import BeamWarming
 import pyplot as bs
 
 
@@ -17,8 +17,8 @@ import pyplot as bs
 if __name__ == "__main__":
     bs.symbolplot(BeamWarming, lambdacursor = True)
 
-    bs.symbolplot(BeamWarming, [0.7,1.4, 1.8], lambdacursor = True)
+    # bs.symbolplot(BeamWarming, [0.7,1.4, 1.8], lambdacursor = True)
 
-    bs.symbolplot(BeamWarming, 1.4)
+    # bs.symbolplot(BeamWarming, 1.4)
 
-    bs.symbolplot(BeamWarming, np.array([0.7, 1, 1.6]))
+    # bs.symbolplot(BeamWarming, np.array([0.7, 1, 1.6]))
